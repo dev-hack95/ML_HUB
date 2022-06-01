8042a47ef771c46ab874d6179800ee9145a642de
Saiprasad Toshatwad <77664251+dev-hack95@users.noreply.github.com>
2022-06-01 13:30:05 +0530

    Add files via upload

---
+++ Logistic Regression(Tensorflow.js)/Logistic_reg.mjs
@@ -0,0 +1,73 @@
+//import * as tf from "@tensorflow/tfjs"
+
+async function load_data() {
+    const csvUrl = './data/iris.csv'
+    const csv_data = tf.data.csv(csvUrl, {
+        columnConfigs: {
+            Species: {
+                isLabel: true
+            }
+        }
+    });
+
+
+    const Features = (await csv_data.columnNames()).length - 1;
+    console.log(Features);
+    const convert_data =
+        csv_data.map(({xs, ys}) => {
+            const lables = [
+                ys.Species == "setosa" ? 1 : 0,
+                ys.Species == "virginica" ? 1 : 0,
+                ys.Species == "versicolor" ? 1 : 0
+            ]
+            return {
+                xs: Object.values(xs),
+                ys: Object.values(lables)
+            };
+        }).batch(10)
+
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+        inputShape: [Features],
+        activation: "sigmoid",
+        units: 5
+    }));
+
+    model.add(tf.layers.dense({
+        activation: "softmax",
+        units: 3
+    }));
+
+    model.compile({
+        loss: "categoricalCrossentropy",
+        optimizer: tf.train.adam(0.01)
+    });
+
+    await model.fitDataset(convert_data,
+        {
+            epochs: 150,
+            callbacks: {
+                onEpochEnd: async (epoch, logs) => {
+                    console.log("Epoch: " + epoch + "Loss: " + logs.loss);
+                }
+            }
+        });
+
+
+    const test_values = tf.tensor2d([5.8, 2.7, 5.1, 1.9], [1, 4]);
+
+    const prediction = model.predict(test_values);
+    const pIndex = tf.argMax(prediction, axis = 1).dataSync();
+
+    const classNames = ["Setosa", "Virfinica", "Versicolor"];
+
+    test_values.print()
+    prediction.print()
+    test_values.dispose();
+    prediction.dispose();
+    console.log(prediction);
+    console.log(classNames[pIndex]);
+
+
+}
+load_data();
